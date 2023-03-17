bdd947d22d087f5a162f2d55ecf94b811736e412
Janette Campbell <campbjp@hotmail.com>
2023-03-17 16:53:58 -0400

    feat: created mad libs game

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-print("    /\\")
-print("   /  \\")
-print("  /    \\")
-print(" /      \\")
-print("/________\\")
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+
+print(f"Roses are {color}")
+print(f"{plural_noun} are blue")
+print(f"I love {celebrity}")
