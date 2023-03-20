a9a9e68d338711a55656170e7885a7d8a9fc106e
Janette Campbell <campbjp@hotmail.com>
2023-03-20 18:02:38 -0400

    chore: lesson on methods

--- app.py
+++ app.py
@@ -1,26 +1,6 @@
-from Question import Question
+from Student import Student
 
-question_prompts = [
-    "What color are apples? \n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
-    "What color are bananas? \n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
-    "What color are strawberries? \n(a) Yellow\n(b) Red\n(c) Blue\n\n"
-]
+student1 = Student("Oscar", "Accounting", 3.1)
+student2 = Student("Phyllis", "Business", 3.8)
 
-questions = [
-    Question(question_prompts[0], "a"),
-    Question(question_prompts[1], "c"),
-    Question(question_prompts[2], "b"),
-]
-
-def run_test(questions):
-    score = 0
-
-    for question in questions:
-        answer = input(question.prompt)
-        if answer == question.answer:
-            score += 1
-
-    print(f"You got {score} / {len(questions)} correct")
-
-
-run_test(questions)
+print(student2.on_honor_roll())
