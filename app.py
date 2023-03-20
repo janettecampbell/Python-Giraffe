78caec032b1c6c6883d537dfed628f345c27fbc2
Janette Campbell <campbjp@hotmail.com>
2023-03-20 11:33:13 -0400

    feat: created simple calculator

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-def cube(num):
-    return num * num * num
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
 
-result = cube(4)
-print(result)
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Invalid operator")
