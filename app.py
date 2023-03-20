c75e58c24649457e05bbe91b85b35872d527c56b
Janette Campbell <campbjp@hotmail.com>
2023-03-20 11:42:14 -0400

    feat: created simple month conversion dictionary

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-num1 = float(input("Enter first number: "))
-op = input("Enter operator: ")
-num2 = float(input("Enter second number: "))
+month_conversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
 
-if op == "+":
-    print(num1 + num2)
-elif op == "-":
-    print(num1 - num2)
-elif op == "/":
-    print(num1 / num2)
-elif op == "*":
-    print(num1 * num2)
-else:
-    print("Invalid operator")
+print(month_conversions.get("Luv", "Not a valid key."))
